42b31d55e625e5768b6e08e7c67d25eaf2309ff8
Remi H. Ho <remyho427@gmail.com>
2024-03-24 23:46:35 +0800

    modified scss import

--- src/client.tsx
+++ src/client.tsx
@@ -2,12 +2,6 @@
 import { h } from "preact";
 import { hydrate } from "preact";
 import App from "./app";
-
-//
-import "./styles/reset.scss";
-import "./styles/global.scss";
-import "./styles/font.scss";
-import "./styles/app.scss";
-import "./styles/homepage.scss";
+import "./styles/main.scss";
 
 hydrate(<App />, document.getElementById("root")!);
