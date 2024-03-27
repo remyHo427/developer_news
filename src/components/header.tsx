d0fe196cc763e17a61ec775ca4fbe24d428bf221
Remi H. Ho <remyho427@gmail.com>
2024-03-27 16:01:25 +0800

    pending work on esbuild scripts

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,26 @@
 /** @jsx h */
 import { h } from "preact";
+import { Link } from "wouter-preact";
 
 const Header = () => {
-    return <div className="header">header</div>;
+    return (
+        <div className="header">
+            <nav className="func-box">
+                <Link to="/news">
+                    <div className="logo">Developer News</div>
+                </Link>
+                <div className="links">
+                    <Link to="/newest">new</Link>
+                    <Link to="/past">past</Link>
+                    <Link to="/ask">ask</Link>
+                    <Link to="/show">show</Link>
+                    <Link to="/jobs">jobs</Link>
+                    <Link to="/submit">submit</Link>
+                </div>
+            </nav>
+            <Link to="/login">login</Link>
+        </div>
+    );
 };
 
 export default Header;
