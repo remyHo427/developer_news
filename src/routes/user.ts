9fe31a651a11f6ef81338d51c940265be15082a5
Remi H. Ho <remyho427@gmail.com>
2024-04-02 22:22:17 +0800

    pending work in routes/user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,23 +1,6 @@
-import { DELETE, GET, POST, PUT, User } from "../types";
+import { POST, GET, User } from "../types";
 import crypto from "crypto";
 
-export const GetUserById = new GET("/user/:name", async (req, res, knex) => {
-    const { name } = req.params as { name: string };
-
-    const table = knex<User>("User");
-    const usr = await table.select().where("name", name).first();
-
-    if (!usr) {
-        res.status(404).send("User not found");
-        return;
-    }
-
-    res.status(200).send({
-        name: usr.name,
-        email: usr.email,
-        createdAt: usr.createdAt,
-    });
-});
 export const CreateUser = new POST(
     "/user",
     async (req, res, knex) => {
@@ -30,23 +13,32 @@ export const CreateUser = new POST(
         };
 
         const table = knex<User>("User");
-        const names = await table.select().where("name", name);
-        const emails = await table.select().where("email", email);
+        const duplicateUser = await table.select().where("name", name).first();
+        const duplicateEmail = await table
+            .select()
+            .where("email", email)
+            .first();
 
         // check
-        if (names.length) {
-            res.status(409).send("Username already exists");
-        } else if (emails.length) {
-            res.status(409).send("Email already registered");
+        if (duplicateUser) {
+            return res.status(409).send("Username already exists");
+        } else if (duplicateEmail) {
+            return res.status(409).send("Email already registered");
         }
 
+        // TODO: email verification
+
         // insert
+        const hashedPassword = crypto
+            .createHash("sha256")
+            .update(password + PEPPER + salt)
+            .digest("hex");
         await table.insert({
             uuid: knex.raw("UUID_TO_BIN(UUID())"),
             name,
             email,
             salt,
-            password: knex.raw('SHA2("?", 256)', [password + PEPPER + salt]),
+            password: hashedPassword,
         });
 
         res.send(200);
@@ -65,9 +57,76 @@ export const CreateUser = new POST(
         },
     },
 );
-export const UpdateUser = new PUT("/user/:name", async (req, res, knex) => {
+export const UserLogin = new POST(
+    "/login",
+    async (req, res, knex) => {
+        const { PEPPER } = process.env;
+        const { login, password } = req.body as {
+            login: string; // login is either email or username
+            password: string;
+        };
+
+        const table = knex<User>("User");
+        const user = await table
+            .select()
+            .where("name", login)
+            .orWhere("email", login)
+            .first();
+
+        if (!user) {
+            res.status(400).send("Invalid username/email or password");
+            return;
+        }
+
+        const hashedPassword = crypto
+            .createHash("sha256")
+            .update(password + PEPPER + user.salt)
+            .digest("hex");
+
+        if (hashedPassword !== user.password) {
+            res.status(400).send("Invalid username/email or password");
+            return;
+        }
+
+        // issue token
+        res.send(200);
+    },
+    {
+        schema: {
+            body: {
+                type: "object",
+                properties: {
+                    login: { type: "string" },
+                    password: { type: "string" },
+                },
+                required: ["login", "password"],
+            },
+        },
+    },
+);
+
+// TODO
+export const GetUserInfo = new GET("/user/:name", async (req, res, knex) => {
+    // TODO: set up this API to return public user info
+    // for display in /user page in frontend
     res.send(404);
 });
-export const DeleteUser = new DELETE("/user/:name", async (req, res, knex) => {
-    res.send(404);
-});
+
+// GET example
+// export const GetUserById = new GET("/user/:name", async (req, res, knex) => {
+//     const { name } = req.params as { name: string };
+
+//     const table = knex<User>("User");
+//     const usr = await table.select().where("name", name).first();
+
+//     if (!usr) {
+//         res.status(404).send("User not found");
+//         return;
+//     }
+
+//     res.status(200).send({
+//         name: usr.name,
+//         email: usr.email,
+//         createdAt: usr.createdAt,
+//     });
+// });
